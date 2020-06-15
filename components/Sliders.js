b51dc369fae7b214488aa0829a95aae2823c03d3
kk <sirohikrishnakant@gmail.com>
2020-06-15 15:47:42 -0500

    Sliders.js

--- components/Sliders.js
+++ components/Sliders.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { View, Text} from 'react-native';
+import { View, Text, Slider} from 'react-native';
 
-export default function Sliders() {
+export default function Sliders({max, unit, step, value, onChange}) {
     return (
         <View>
-            <Text>Sliders</Text>
+            <Slider
+                step={step}
+                value={value}
+                maximumValue={max}
+                minimumValue={0}
+                onValueChange={onChange}
+            />
+            <View>
+                <Text>
+                    {value}
+                </Text>
+                <Text>
+                    {unit}
+                </Text>
+            </View>
         </View>
     )
 }
