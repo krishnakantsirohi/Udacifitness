d6d2fb5fa640aea5616728f94c86a87712049e79
kk <sirohikrishnakant@gmail.com>
2020-06-17 15:15:53 -0500

    Added Entry Detail

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -2,25 +2,19 @@ import React from 'react';
 import {View, Text} from 'react-native';
 
 class EntryDetail extends React.Component{
-
-    setTitle = (entryId) => {
-        if (!entryId) return;
-
-        const year = entryId.slice(0, 4)
-        const month = entryId.slice(5, 7)
-        const day = entryId.slice(8)
-
-        this.props.navigation.setOptions({
-            title: `${month}/${day}/${year}`
-        });
-    };
+    componentDidMount() {
+        const {entryID} = this.props.route.params
+        const year = entryID.slice(0, 4)
+        const month = entryID.slice(5, 7)
+        const day = entryID.slice(8)
+        this.props.navigation.setOptions({title: `${month}/${day}/${year}`})
+    }
 
     render() {
-        const {entryId} = this.props.route.params;
-        this.setTitle(entryId);
+        const {entryID} = this.props.route.params;
         return (
             <View>
-                <Text>Entry Details - {this.props.route.params.entryID}</Text>
+                <Text>Entry Details - {entryID}</Text>
             </View>
         )
     }
